112a99217183f8049935085bd72312f619c0e1fe
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-25 03:16:04 -0500

    timestamp superscript mode

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ program
 	.option('-t, --token <X-Plex-Token>', 'Set Plex server token')
 	.option('-a, --address <Plex server IP / address & port>', 'Set Plex server address, including protocol and port (Example: http://127.0.0.1:32400)')
 	.option('-s, --short', 'Enable "short" mode. Disables subtitle from appearing for tracks.')
+	.option('-u, --superscript', 'Enable small timestamps. Uses superscript characters.')
 	.option('-p, --polling-rate <Polling rate in milliseconds>', 'Set polling rate for contacting Plex API in milliseconds (Default: 500ms)')
 	.helpOption('-h, --help', 'Show help information')
 	.parse()
@@ -94,13 +95,22 @@ async function getPlexSessions() {
 			viewOffsetMs = session.viewOffset
 		}
 
-		const durationTimestamp = secondsToTimestamp(session.duration / 1000)
-		const currentTimestamp = secondsToTimestamp(viewOffsetMs / 1000)
+		let durationTimestamp = secondsToTimestamp(session.duration / 1000)
+		let currentTimestamp = secondsToTimestamp(viewOffsetMs / 1000)
+		if (options.superscript) {
+			durationTimestamp = toSuperScript(secondsToTimestamp(session.duration / 1000))
+			currentTimestamp = toSuperScript(secondsToTimestamp(viewOffsetMs / 1000))
+		}
+
+		let timestampSeparator = '/'
+		if (options.superscript) {
+			timestampSeparator = ''
+		}
 
 		let incompleteMessage = `${emoji}${title}${emoji}${newline}${subtitle}`
-		let chatboxMessage = `${incompleteMessage} (${year})\n${currentTimestamp} / ${durationTimestamp}`
+		let chatboxMessage = `${incompleteMessage} (${year})\n${currentTimestamp} ${timestampSeparator} ${durationTimestamp}`
 		if (options.short && session.type === 'track')
-			chatboxMessage = `${incompleteMessage}${currentTimestamp} / ${durationTimestamp}`
+			chatboxMessage = `${incompleteMessage}${currentTimestamp} ${timestampSeparator} ${durationTimestamp}`
 
 		// VRChat has max Chatbox length of 144 characters. Chop off extra characters *before* year and timestamp, and add '...'
 		if (chatboxMessage.length > 144) {
@@ -111,7 +121,7 @@ async function getPlexSessions() {
 		// Avoid VRChat spam by not sending the same message twice in less than 3 seconds.
 		if (lastOSCMessage === incompleteMessage && new Date().getTime() - lastOSCMessageTimeMs < 3000)
 			return;
-		
+
 		oscClient.send('/chatbox/input', chatboxMessage, true, false)
 		console.log(chalk`{cyan [${new Date().toLocaleTimeString()}]} {white 💬: ${chatboxMessage.replaceAll('\n', ' | ')}}`)
 
@@ -139,15 +149,70 @@ async function getMediaYear(ratingKey) {
 }
 
 function secondsToTimestamp(seconds) {
+	let timestampDivider = ':'
+	if (options.superscript) {
+		timestampDivider = `'`
+	}
+
 	const h = Math.floor(seconds / 3600).toString(),
 				m = Math.floor(seconds % 3600 / 60).toString(),
 				s = Math.floor(seconds % 60).toString().padStart(2,'0')
-	
+
 	// Show hours and pad minutes only if over an hour.
 	if (h == 0)
-		return `${m}:${s}`;
-	
-	return `${h}:${m.padStart(2,'0')}:${s}`;
+		return `${m}${timestampDivider}${s}`;
+
+	return `${h}${timestampDivider}${m.padStart(2,'0')}${timestampDivider}${s}`;
+}
+
+var SUPERSCRIPTS = {
+	' ': ' ',
+	'0': '⁰',
+	'1': '¹',
+	'2': '²',
+	'3': '³',
+	'4': '⁴',
+	'5': '⁵',
+	'6': '⁶',
+	'7': '⁷',
+	'8': '⁸',
+	'9': '⁹',
+	'+': '⁺',
+	'-': '⁻',
+	'a': 'ᵃ',
+	'b': 'ᵇ',
+	'c': 'ᶜ',
+	'd': 'ᵈ',
+	'e': 'ᵉ',
+	'f': 'ᶠ',
+	'g': 'ᵍ',
+	'h': 'ʰ',
+	'i': 'ⁱ',
+	'j': 'ʲ',
+	'k': 'ᵏ',
+	'l': 'ˡ',
+	'm': 'ᵐ',
+	'n': 'ⁿ',
+	'o': 'ᵒ',
+	'p': 'ᵖ',
+	'r': 'ʳ',
+	's': 'ˢ',
+	't': 'ᵗ',
+	'u': 'ᵘ',
+	'v': 'ᵛ',
+	'w': 'ʷ',
+	'x': 'ˣ',
+	'y': 'ʸ',
+	'z': 'ᶻ'
+}
+
+function toSuperScript(string) {
+	return string.split('').map(function(character) {
+		if (character in SUPERSCRIPTS) {
+			return SUPERSCRIPTS[character];
+		}
+		return character;
+	}).join('')
 }
 
 setInterval(() => {
